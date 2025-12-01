e0f5f0965bd9fd5bccfdb3561fcb4d59aa7b7506
Noah Kvirkvelia <kvirkvelianoah@gmail.com>
2025-12-02 02:57:29 +0400

    dark theme update

--- src/app.js
+++ src/app.js
@@ -25,3 +25,20 @@ const observer = new IntersectionObserver((entries) => {
 
 circle.forEach(circle => observer.observe(circle));
 
+const btn = document.getElementById('toggle');
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme) {
+    document.documentElement.setAttribute('data-theme', savedTheme);
+}
+
+btn.addEventListener('click', () => {
+    const current = document.documentElement.getAttribute('data-theme');
+    const next = current === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', next);
+    localStorage.setItem('theme', next);
+
+});
+
+
+
